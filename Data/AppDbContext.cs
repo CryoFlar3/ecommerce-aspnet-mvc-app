61f377785983bdd4ec266951a5405b45440d67f7
Nyjel Dukes <ndukes@computermentors.org>
2022-10-31 15:28:05 -0400

    Added Tables for Orders, OrderItems, and ShoppingCart

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -28,5 +28,10 @@ namespace eTickets.Data
         public DbSet<Producer> Producers { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
+
+        //Orders related tables
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
